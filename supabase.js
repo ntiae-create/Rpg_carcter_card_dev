01e770a091c5002b31623be54dcd5e80bd7607e0
ntiae-create <antiaero98@gmail.com>
2026-09-13 19:06:04 -0300

    Update Supabase key in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://bjkbfxcmyihdruqrwsdf.supabase.co";
 
-const SUPABASE_KEY = "SUA_PUBLISHABLE_KEY_AQUI";
+const SUPABASE_KEY = "sb_publishable_j2Qi8rg5sr3qvtq6-HAvkQ_Deurk34_";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
